3d983f21116b498037a2f12fca3d9da1ca636fba
ybendavi <yohan.p002@gmail.com>
2024-11-27 17:51:53 +0100

    histogramme implemente

--- test.py
+++ test.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import math
 
+import matplotlib.pyplot as plt
+
 
 def calculate_count(column):
     count = 0
@@ -63,10 +65,11 @@ def describe_col(column):
 
 
 def main():
+# Exemple de données
     datas = None
     datas_train = None
     try:
-        datas = pd.read_csv("datasets/dataset_test.csv")
+        datas = pd.read_csv("datasets/dataset_train.csv")
         datas_train = pd.read_csv("datasets/dataset_train.csv")
     except Exception as e:
         print("Something went wrong with the csv file:", str(e))
@@ -77,10 +80,40 @@ def main():
     #print(datas_train.dtypes)
     numerical_features = datas[[col for col in datas.columns if datas[col].dtype == 'float64']]
     numerical_features = numerical_features.dropna(axis=1, how='all')
-    numerical_features_col = numerical_features.columns
-    final_df = pd.DataFrame(index=["Count", "Mean", "Std", "Min", "25%", "50%", "75%", "Max"])
-    for col in numerical_features:
-        describe_col(numerical_features[col])
+    houses = datas['Hogwarts House'].unique()
+    bins = int(math.sqrt(numerical_features.shape[0]))
+    nb_col = numerical_features.shape[1]
+    fig, axes = plt.subplots(nrows=(int(nb_col / 4) + 1), ncols=4, figsize=(20, 12))
+    i = 0
+    j = 0
+    print(fig)
+    for feature in numerical_features:
+        for cat in houses:
+            # Ici je demande a mon dataframe de renvoyer les lignes dont la hh est celle que je traite
+            subset = datas[datas['Hogwarts House'] == cat] 
+            axes[j,i].hist(subset[feature], bins=10, alpha=0.5, label=f'{cat}', edgecolor='black')
+
+        axes[j,i].set_title(f'{feature}')
+        #axes[j,i].xlabel(feature)
+        #axes[j,i].ylabel('Fréquence')
+        #axes[j,i].legend()
+        i += 1
+        if i == 4:
+            j += 1
+            i = 0
+    while i < 4:
+        axes[j,i].axis('off')
+        i += 1
+    handles, labels = axes[0, 0].get_legend_handles_labels()
+    fig.legend(handles, labels, loc='lower center', ncol=1, fontsize=12)
+    #fig.suptitle('Repartition des features par Hogwarts House')
+    plt.tight_layout()
+    plt.show()
+    #print(numerical_features.describe())
+    # numerical_features_col = numerical_features.columns
+    # final_df = pd.DataFrame(index=["Count", "Mean", "Std", "Min", "25%", "50%", "75%", "Max"])
+    # for col in numerical_features:
+    #     describe_col(numerical_features[col])
     #print(numerical_features.dtypes)
     #print("numfd:", numerical_features)
     #numerical_features_train = datas_train.select_dtypes(include=['float'])
